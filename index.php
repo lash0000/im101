3141a23d9072e57e929020ede9e4e4838d625588
lash0000 <k80308392@gmail.com>
2024-03-25 08:19:56 +0800

    :rocket: rollup

--- index.php
+++ index.php
@@ -1,10 +1,22 @@
-<h1>Home page</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
 
-<form action="phpbasicform.php" method="post">
-  <label for="fname">First Name:</label><br>
-  <input type="text" id="fname" name="fname" required><br>
-  <label for="lname">Last Name:</label><br>
-  <input type="text" id="lname" name="lname" required><br><br>
+<body>
+  <section>
+    <h1>asdasdsadas</h1>
+    <form action="phpbasicform.php" method="post">
+      <label for="fname">First Name:</label><br>
+      <input type="text" id="fname" name="fname" required><br>
+      <label for="lname">Last Name:</label><br>
+      <input type="text" id="lname" name="lname" required><br><br>
 
-  <input type="submit" value="Submit">
-</form>
+      <input type="submit" value="Submit">
+    </form>
+  </section>
+</body>
+</html>
