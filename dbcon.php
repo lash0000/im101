e388f8ba0a1e44ff0b681c75a1cb97d5fc7c747e
lash0000 <k80308392@gmail.com>
2024-04-11 06:02:19 +0800

    still WIP

--- dbcon.php
+++
@@ -1,5 +0,0 @@
-<?php
-    $oracle_dbname = "im101";
-    $oracle_pass = "Eyelashmelon01";
-    $host = "localhost/orcl"
-?>
--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<?php
+$oracle_dbname = "im101";
+$oracle_pass = "Eyelashmelon01";
+$oracle_host = "localhost/orcl";
+
+$conn = oci_connect($oracle_dbname, $oracle_pass, $oracle_host);
+if (!$conn) {
+    $e = oci_error();
+    trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+} else {
+    echo "Connected to Oracle database!";
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
