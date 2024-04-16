571163fb4bd8955ddf5d316f7b6cf98ea0a23d9e
lash0000 <k80308392@gmail.com>
2024-04-16 22:22:54 +0800

    :heart: ready to responsive

--- views/client/login/index.php
+++ views/client/login/index.php
@@ -38,15 +38,15 @@
                         <input type="password" id="password" name="password" required>
                     </div>
                     <button type="submit" style="margin-bottom: 12px;">Sign in</button>
+                    <aside class="login-options">
+                        <span>New to Treiven? <a href="">Sign Up</a></span>
+                    </aside>
                 </form>
-                <aside class="login-options">
-                    <span>New to Treiven? <a href="">Sign Up</a></span>
-                </aside>
                 <aside class="login-info">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18">
-                        <g fill="#212121" class="nc-icon-wrapper">
-                            <path d="M9.305,1.848l5.25,1.68c.414,.133,.695,.518,.695,.952v6.52c0,3.03-4.684,4.748-5.942,5.155-.203,.066-.413,.066-.616,0-1.258-.407-5.942-2.125-5.942-5.155V4.48c0-.435,.281-.82,.695-.952l5.25-1.68c.198-.063,.411-.063,.61,0Z" fill="none" stroke="#212121" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"></path>
-                            <polyline points="6.497 9.75 8.106 11.25 11.503 6.75" fill="none" stroke="#212121" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" data-color="color-2"></polyline>
+                        <g fill="#F47B37" class="nc-icon-wrapper">
+                            <path d="M9.305,1.848l5.25,1.68c.414,.133,.695,.518,.695,.952v6.52c0,3.03-4.684,4.748-5.942,5.155-.203,.066-.413,.066-.616,0-1.258-.407-5.942-2.125-5.942-5.155V4.48c0-.435,.281-.82,.695-.952l5.25-1.68c.198-.063,.411-.063,.61,0Z" fill="none" stroke="#F47B37" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"></path>
+                            <polyline points="6.497 9.75 8.106 11.25 11.503 6.75" fill="none" stroke="#F47B37" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" data-color="color-2"></polyline>
                         </g>
                     </svg>
                     <span>Agile Security - We ensure your data is secure and private all times.</span>
@@ -55,7 +55,7 @@
         </section>
         <section class="login-banner">
             <div class="banner-wrap">
-                
+                <a href="https://www.facebook.com/permalink.php?story_fbid=pfbid02RfSs8YPx9wh3SZdwkYJwPXu5JscRxixPybKaeRwoQrTGrMoDQP7yG7NUg7AXbsMDl&id=100091824182246" target="_blank" rel="noopener noreferrer"><span>Bento Cake w/ Flower by Tréiven</span></a>
             </div>
         </section>
     </main>
