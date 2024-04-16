f0248f40414649f7de58c01b0482ef996585d4ca
lash0000 <k80308392@gmail.com>
2024-04-17 06:34:06 +0800

    :tada: sign up

--- views/client/register/index.php
+++ views/client/register/index.php
@@ -21,6 +21,45 @@
 
 <body>
 
+    <main class="login-grid-wrapper">
+        <section class="login-container">
+            <div class="login-wrapper">
+                <header class="login-title">
+                    <label>Sign up</label>
+                    <p>Please enter your details to sign up</p>
+                </header>
+                <form id="loginForm" method="post">
+                    <div class="form-group">
+                        <label for="email">Email address</label>
+                        <input type="email" id="email" name="email" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" id="password" name="password" required>
+                    </div>
+                    <button type="submit" style="margin-bottom: 12px;">Sign in</button>
+                    <aside class="login-options">
+                        <span>Already have an account? <a href="../login/">Sign In</a></span>
+                    </aside>
+                </form>
+                <aside class="login-info">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18">
+                        <g fill="#F47B37" class="nc-icon-wrapper">
+                            <path d="M9.305,1.848l5.25,1.68c.414,.133,.695,.518,.695,.952v6.52c0,3.03-4.684,4.748-5.942,5.155-.203,.066-.413,.066-.616,0-1.258-.407-5.942-2.125-5.942-5.155V4.48c0-.435,.281-.82,.695-.952l5.25-1.68c.198-.063,.411-.063,.61,0Z" fill="none" stroke="#F47B37" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"></path>
+                            <polyline points="6.497 9.75 8.106 11.25 11.503 6.75" fill="none" stroke="#F47B37" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" data-color="color-2"></polyline>
+                        </g>
+                    </svg>
+                    <span>Agile Security - We ensure your data is secure and private all times.</span>
+                </aside>
+            </div>
+        </section>
+        <section class="signup-banner">
+            <div class="signup-banner-wrap">
+                <a href="https://www.facebook.com/permalink.php?story_fbid=pfbid02vS4u16zUiLidRixRTpmY7sUajQhKNzkiJT35dS5YsX1nEUv6mjnjQyjtg9ygtZJ7l&id=100091824182246" target="_blank" rel="noopener noreferrer"><span>February Treats by Tréiven</span></a>
+            </div>
+        </section>
+    </main>
+
 </body>
 <script src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/gsap.min.js"></script>
 <script src=""></script>
